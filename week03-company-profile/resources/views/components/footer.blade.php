3278efa1a1fe8dde8ef8e0b76ac2078b47663423
Contado I, Denzelle A. <denzelle.a.contado.l@gmail.com>
2026-08-15 19:51:09 +0800

    feat: update footer

--- week03-company-profile/resources/views/components/footer.blade.php
+++ week03-company-profile/resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-gray-900 text-gray-400 text-center p-6 mt-12">
-    <p>&copy; {{ date('Y') }} TechCorp Solutions. All rights reserved.</p>
-    <p class="text-sm mt-2">Email: contact@techcorp.com | Phone: +1 (555) 019-2834</p>
+<footer class="bg-black text-gray-400 text-center p-8 mt-12 border-t border-gray-800">
+    <p>&copy; {{ date('Y') }} RedLine Creative Studios. All rights reserved.</p>
+    <p class="text-sm mt-2 text-gray-500">The Creative Hub, Laguna, Philippines | create@redlinestudios.art</p>
 </footer>
